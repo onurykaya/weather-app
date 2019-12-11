7746405a44404b4d04df8cb15d8c1b01546b8557
Onur Yerlikaya <onur.yerlikaya1@gmail.com>
2019-12-11 16:43:26 +0300

    version 2

--- src/components/Form.js
+++ src/components/Form.js
@@ -15,14 +15,14 @@ export default function Form(props) {
                 <form onSubmit={props.submitWeather}>
                     <div> {props.error ? error() : null } </div>
                     <div className="row mt-4">  
-                        <div className="col-md-3 offset-md-2">
+                        < div className = "col-md-3 offset-md-2 mt-3" >
                             <input type="text" className="form-control" name="city" autoComplete="off" placeholder="City" />
                         </div>
-                        <div className="col-md-3">
+                        <div className="col-md-3 mt-3">
                             <input type="text" className="form-control" name="country" autoComplete="off" placeholder="Country" />
                         </div>
-                        <div className="col-md-3 mt-md-0 text-md-left">
-                            <button className="btn btn-warning mb-4">Ara</button>
+                        < div className = "col-md-3 mt-md-0 text-md-left" >
+                            <button className="btn btn-warning mb-4 mt-3">Ara</button>
                         </div>
                     </div>
                 </form>
